77deb643d86857258c123e6608cce5136f29e4a6
Mamad <m.shekari79b5@gmail.com>
2022-10-08 19:18:21 +0330

    [BugFix] Fix Some Bugs

--- src/badi_user/ui/member_views.py
+++ src/badi_user/ui/member_views.py
@@ -1,26 +1,26 @@
 from badi_utils.dynamic import DynamicCreateView, DynamicListView, DynamicUpdateView
-from badi_user.filter import UserListFilter
+from badi_user.filter import UserListFilter, MemberListFilter
 from django.contrib.auth import get_user_model
+from badi_user.ui.forms import user_form
 
 User = get_user_model()
-
-from badi_user.ui.forms import user_form
 
 
 class MemberListView(DynamicListView):
     model = User
     model_name = 'عضو'
-    datatable_cols = ['#', "انتخاب", 'تصویر', 'نام کاربری', 'نام', 'نام خانوادگی', 'موجودی کیف پول']
+    datatable_cols = User.get_datatable_cols('MemberListView')
     template_name = 'member/member_list.html'
 
     def get_extra_context(self, context):
-        context['filters'] = UserListFilter(self.request.POST)
+        context['filters'] = MemberListFilter(self.request.POST)
         return super().get_extra_context(context)
 
 
 class MemberCreateView(DynamicCreateView):
     model = User
     model_name = 'عضو'
+    api_url = '/api/v1/member/'
     form = user_form(User.get_form_fields('member_create'))
     template_name = 'member/member_create.html'
     datatableEnable = False
