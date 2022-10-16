935385a145ff28f04c1c13f9979ca281ef5cab5d
Mamad <m.shekari79b5@gmail.com>
2022-10-16 11:38:17 +0330

    [Feature][BugFix][Dist][Version] Add INIT and Fixes

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='badi_users',
+    version='0.3.1',
+    author='Mohammad Shekari Badi',
+    author_email='Ad.BadiDesign@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/BadiDesign/badi_utils',
+    license='LICENSE',
+    description='Python Django Users',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
