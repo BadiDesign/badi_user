c4b763e244ac019605f4821ae4326fea1bec7e97
Mamad <m.shekari79b5@gmail.com>
2024-04-23 16:53:58 +0330

    [Feature][BugFix][Dist][Version] Fix Sms of IPPANEL

--- src/badi_utils/sms.py
+++ src/badi_utils/sms.py
@@ -1,23 +1,20 @@
 from os import getenv
 
 import requests
+import json
 
 SMS_PANNEL_TYPE = getenv('SMS_PANNEL_TYPE', 'ippanel')
 
-SMS_SEND_URL = getenv('SMS__IP_PANEL_URL')
-SMS_POST_URL = getenv('SMS_POST_URL', 'http://ippanel.com/api/select')
 ORIGINATOR = getenv('SMS__IP_PANEL_ORGINATOR')
-SMS__USERNAME = getenv('SMS__USERNAME')
-SMS__PASSWORD = getenv('SMS__PASSWORD')
 SMS_ENABLE = getenv('SMS__ENABLE')
-SMS__IPPANEL_F_NUM_VERIFY_CODE = getenv('SMS__IPPANEL_F_NUM_VERIFY_CODE')
 SMS__IPPANEL_P_ID_VERIFY_CODE = getenv('SMS__IPPANEL_P_ID_VERIFY_CODE')
-SMS__IPPANEL_P_ID_FORGOT_LINK = getenv('SMS__IPPANEL_P_ID_FORGOT_LINK')
 SMS__IPPANEL_PATTERN_CODE_FORGOT_LINK = getenv('SMS__IPPANEL_PATTERN_CODE_FORGOT_LINK')
 SMS__IPPANEL_PARAM_NAME = getenv('SMS__IPPANEL_PARAM_NAME', "v-code")
 SMS__IPPANEL_FORGOT_LINK_V1 = getenv('SMS__IPPANEL_FORGOT_LINK_V1', "token")
 SMS__IPPANEL_FORGOT_LINK_V2 = getenv('SMS__IPPANEL_FORGOT_LINK_V2', "hash")
-SMS__IPPANEL_FROM_NUMBER_FORGOT_LINK = getenv('SMS__IPPANEL_FROM_NUMBER_FORGOT_LINK', SMS__IPPANEL_F_NUM_VERIFY_CODE)
+SMS__IPPANEL_SENDER = getenv('SMS__IPPANEL_SENDER', "3000505")
+SMS__IPPANEL_SEND_URL = getenv('SMS__IPPANEL_SEND_URL', "http://api2.ippanel.com/api/v1/sms/pattern/normal/send")
+SMS__IPPANEL_SEND_URL_FORGOT = getenv('SMS__IPPANEL_SEND_URL_FORGOT', SMS__IPPANEL_SEND_URL)
 
 
 class IpPanelSms:
@@ -32,20 +29,22 @@ class IpPanelSms:
             return
         for i in range(10):
             try:
-                result = requests.get(
-                    SMS_SEND_URL,
-                    params={
-                        "apikey": ORIGINATOR,
-                        "fnum": SMS__IPPANEL_F_NUM_VERIFY_CODE,
-                        "tnum": self.phone_number,
-                        "pid": SMS__IPPANEL_PATTERN_CODE_FORGOT_LINK,
-                        "p1": SMS__IPPANEL_FORGOT_LINK_V1,
-                        "v1": str(token_id_hash),
-                        "p2": SMS__IPPANEL_FORGOT_LINK_V2,
-                        "v2": str(hash_code),
-                    }, headers={
-                        "Content-Type": "application/json",
-                    })
+                url = SMS__IPPANEL_SEND_URL_FORGOT
+                payload = json.dumps({
+                    "code": SMS__IPPANEL_PATTERN_CODE_FORGOT_LINK,
+                    "recipient": self.phone_number,
+                    "sender": SMS__IPPANEL_SENDER,
+                    "variable": {
+                        SMS__IPPANEL_FORGOT_LINK_V1: str(token_id_hash),
+                        SMS__IPPANEL_FORGOT_LINK_V2: str(hash_code),
+                    }
+                })
+                headers = {
+                    'apikey': ORIGINATOR,
+                    'Content-Type': 'application/json'
+                }
+                response = requests.request("POST", url, headers=headers, data=payload)
+                print("Response:", response)
                 return True
             except Exception as e:
                 print(e)
@@ -58,20 +57,21 @@ class IpPanelSms:
             return
         for i in range(10):
             try:
-
-                x = requests.get(
-                    SMS_SEND_URL,
-                    params={
-                        "apikey": ORIGINATOR,
-                        "fnum": SMS__IPPANEL_F_NUM_VERIFY_CODE,
-                        "tnum": self.phone_number,
-                        "pid": SMS__IPPANEL_P_ID_VERIFY_CODE,
-                        "p1": SMS__IPPANEL_PARAM_NAME,
-                        "v1": text
-                    }, headers={
-                        "Content-Type": "application/json",
-                    })
-                print(x)
+                url = SMS__IPPANEL_SEND_URL
+                payload = json.dumps({
+                    "code": SMS__IPPANEL_P_ID_VERIFY_CODE,
+                    "recipient": self.phone_number,
+                    "sender": SMS__IPPANEL_SENDER,
+                    "variable": {
+                        SMS__IPPANEL_PARAM_NAME: text,
+                    }
+                })
+                headers = {
+                    'apikey': ORIGINATOR,
+                    'Content-Type': 'application/json'
+                }
+                response = requests.request("POST", url, headers=headers, data=payload)
+                print("Response:", response)
                 return True
             except Exception as e:
                 print(e)
@@ -84,17 +84,19 @@ class IpPanelSms:
             return
         for i in range(10):
             try:
-                default_params = {
-                    "apikey": ORIGINATOR,
-                    "fnum": SMS__IPPANEL_F_NUM_VERIFY_CODE,
-                    "tnum": self.phone_number,
+                url = SMS__IPPANEL_SEND_URL
+                payload = {
+                    "recipient": self.phone_number,
+                    "sender": SMS__IPPANEL_SENDER,
                 }
-                default_params.update(params)
-                result = requests.get(
-                    SMS_SEND_URL,
-                    params=default_params, headers={
-                        "Content-Type": "application/json",
-                    })
+                payload.update(params)
+                payload = json.dumps(payload)
+                headers = {
+                    'apikey': ORIGINATOR,
+                    'Content-Type': 'application/json'
+                }
+                response = requests.request("POST", url, headers=headers, data=payload)
+                print("Response:", response)
                 return True
             except Exception as e:
                 print(e)
